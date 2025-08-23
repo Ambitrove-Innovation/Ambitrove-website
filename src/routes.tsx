5ac06d10d1302dab236eb0371a2ea6eb8879413d
Kamogelo <ekmogasoa@gmail.com>
2025-08-23 20:06:53 +0200

    Added nav bar component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./features/home/homePage";
+import Navbar from "./components/Navbar";
 
 const Routespath = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
       </Routes>
