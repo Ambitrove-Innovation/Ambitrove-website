ada1b4fa59ec56509668f855b930c0d53f5a95cc
Kamogelo <ekmogasoa@gmail.com>
2025-08-23 02:57:17 +0200

    Add features folder for different page-section

---
+++ src/features/home/homePage.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Homepage = () => {
+  return (
+    <div>
+      Homepage
+      <h1>Welcome to Ambitrove</h1>
+      <Button variant={"secondary"}>We live in a society</Button>
+    </div>
+  );
+};
+
+export default Homepage;
