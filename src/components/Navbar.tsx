8931cdac1ff76119cd66781fe266cca58c17afaa
Kamogelo <ekmogasoa@gmail.com>
2025-08-25 04:15:04 +0200

    Updated navbar max-width and design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className="text-black py-6">
-      <nav className="bg-white container mx-auto flex justify-evenly items-center rounded-full p-2.5 shadow-lg border-b-2 border-cyan-500 shadow-cyan-500/50">
+      <nav className="bg-white container mx-auto flex  max-w-4xl justify-evenly items-center rounded-full p-2.5 shadow-lg border-b-2 border-green-400 shadow-teal-500/50">
         <label aria-label="Company logo" className="flex items-center gap-1.5">
-          <span className="font-semibold text-2xl flex">
+          <span className="font-bold text-2xl flex">
             <BirdIcon /> Ambitrove
           </span>
         </label>
-        <ul className="flex justify-evenly gap-3.5 font-medium text-lg">
+        <ul className="flex justify-evenly gap-3.5 text-[1.3rem] font-semibold">
           <li>
             <Link to="/">Home</Link>
           </li>
@@ -24,7 +24,7 @@ const Navbar = () => {
             <Link to="/services">Services</Link>
           </li>
         </ul>
-        <button className="bg-gray-800 outline-2 outline-cyan-400 font-medium text-white  px-4 py-2 rounded-sm ">
+        <button className="bg-gradient outline-1 outline-cyan-300 font-medium text-white w-30  px-4 py-2 rounded-sm ">
           Follow us
         </button>
       </nav>
