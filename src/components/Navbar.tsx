2e025a17afe7593b919edb8f564f3f44ef670b4e
Kamogelo <ekmogasoa@gmail.com>
2025-08-25 04:30:25 +0200

    Added linkedin link & icon with hover-effects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,14 @@
-import { BirdIcon } from "lucide-react";
+import { BirdIcon, LinkedinIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header className="text-black py-6">
       <nav className="bg-white container mx-auto flex  max-w-4xl justify-evenly items-center rounded-full p-2.5 shadow-lg border-b-2 border-green-400 shadow-teal-500/50">
-        <label aria-label="Company logo" className="flex items-center gap-1.5">
+        <label
+          aria-label="Company logo"
+          className="flex items-center gap-1.5 hover:scale-105 transition-transform duration-300 ease-in-out"
+        >
           <span className="font-bold text-2xl flex">
             <BirdIcon /> Ambitrove
           </span>
@@ -24,8 +27,18 @@ const Navbar = () => {
             <Link to="/services">Services</Link>
           </li>
         </ul>
-        <button className="bg-gradient outline-1 outline-cyan-300 font-medium text-white w-30  px-4 py-2 rounded-sm ">
-          Follow us
+        <button className="bg-gradient outline-1 outline-cyan-300 font-medium text-white w-fit px-4 py-2 rounded-sm shadow-md shadow-black hover:scale-105 transition-transform duration-300 ease-in-out">
+          <Link
+            to="https://www.linkedin.com/company/ambitrove-innovation"
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" flex items-center gap-2"
+          >
+            Follow us
+            <span className="text-teal-500  delay-200 h-6 hover:text-white w-6 ">
+              <LinkedinIcon />
+            </span>
+          </Link>
         </button>
       </nav>
     </header>
