d4bfbfa3d4fc8267e8e228227a6bb23d45fa8b4f
Kamogelo <ekmogasoa@gmail.com>
2025-08-25 04:16:36 +0200

    Updated Hero-section design temporary

--- src/features/home/homePage.tsx
+++ src/features/home/homePage.tsx
@@ -1,19 +1,32 @@
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
 
 const Homepage = () => {
   return (
-    <div className="space-y-4">
-      <h1 className="text-4xl font-bold">Welcome to Ambitrove</h1>
+    <div className="space-y-4 text-center flex flex-col justify-center items-center">
+      <Button
+        variant={"default"}
+        className="w-fit outline-1 outline-double outline-gray-500 rounded-full shadow-md shadow-cyan-500/50"
+      >
+        ✓ Trusted Innovative Solutions
+      </Button>
+      <h1 className="text-5xl font-bold text-gradient w-200 bg-clip-text text-transparent bg-gradient-to-r from-black via-white to-teal-500">
+        Enhancing your ideas with Innovative digital Solutions
+      </h1>
+      <p className="font-light text-2xl w-120 py-5">
+        You think it, we build it - with precision and creative digital
+        solutions that build to scale.
+      </p>
       <Button
         variant={"default"}
         disabled={true}
-        className="outline-dashed outline-1 outline-white w-fit "
+        className="outline-solid outline-1 bg-transparent outline-gray-500 w-35 "
       >
-        Page under construction
+        <Link to="/contact">Contact Us</Link>
       </Button>
 
-      <div className="text-gray-500 animate-pulse ">
-        page content coming soon...
+      <div className="text-gray-500 animate-pulse py-16">
+        <span>page content coming soon...</span>
       </div>
     </div>
   );
