dee02f089aa4ddf3970083c1c1f61428e1add624
Kamogelo <ekmogasoa@gmail.com>
2025-08-26 18:35:22 +0200

    added GSAP animation on button element

--- ambitrove-prototype/src/App.tsx
+++ ambitrove-prototype/src/App.tsx
@@ -1,17 +1,32 @@
 import { useState } from "react";
 import { Button } from "./components/ui/button";
 import TimelineComponent from "./TimelineComponent";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
 
 function App() {
   const [count, setCount] = useState(0);
+
+  useGSAP(() => {
+    gsap.from(".btn", {
+      x: 250,
+      duration: 2,
+      repeat: -1,
+      yoyo: true,
+      ease: "bounce.inOut",
+    });
+  }, []);
 
   return (
     <>
       {" "}
       <div className="card">
-        <h1>Vite React</h1>
+        <h1 className="text-2xl font-bold heading-title">
+          Ambitrove prototype
+        </h1>
 
         <Button
+          className="btn"
           variant={"secondary"}
           onClick={() => setCount((count) => count + 1)}
         >
