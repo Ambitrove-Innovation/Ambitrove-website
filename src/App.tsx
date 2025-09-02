36da0d3a417a8c69a8d2a5615abddba9fe9db5d2
Kamogelo <ekmogasoa@gmail.com>
2025-09-02 14:52:54 +0200

    added gsap 'ScrollTrigger' & 'SplitText'

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import "./index.css";
 import Routespath from "./routes";
+import { gsap } from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
 
+gsap.registerPlugin(ScrollTrigger, SplitText);
 function App() {
   return <Routespath />;
 }
