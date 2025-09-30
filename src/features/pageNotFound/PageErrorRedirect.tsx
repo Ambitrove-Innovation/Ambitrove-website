9543a3b2755ee1a34177c22288cc09f13fd905d0
Kamogelo <ekmogasoa@gmail.com>
2025-09-30 21:39:05 +0200

    Implemented page not found

---
+++ src/features/pageNotFound/PageErrorRedirect.tsx
@@ -0,0 +1,12 @@
+
+const PageErrorRedirect = () => {
+  return (
+    <div>
+      <h1>Page Not Found</h1>
+      <p>We're sorry, but the page you were looking for doesn't exist.</p>
+      <a href="/">Go back to the homepage</a>
+    </div>
+  )
+}
+
+export default PageErrorRedirect
