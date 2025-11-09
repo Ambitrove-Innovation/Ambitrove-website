33119fb1c041ce27d1708b75ba494cb0af92fd84
Kamogelo <ekmogasoa@gmail.com>
2025-11-09 09:31:25 +0200

    Implemented JobListing & BlogPost  type declaration Interface

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -18,3 +18,25 @@ interface FAQItem {
   question: string;
   answer: string;
 }
+
+interface JobListing {
+  id: number;
+  title: string;
+  department: string;
+  location: string;
+  type: string;
+  salary: string;
+  description: string;
+  requirements: string[];
+}
+
+interface BlogPost {
+  id: number;
+  title: string;
+  excerpt: string;
+  category: string;
+  date: string;
+  readTime: string;
+  image: string;
+  author: string;
+}
