13629415c6a0cdb4c11f6f5a438268fc0d7b2df9
Kamogelo <ekmogasoa@gmail.com>
2025-12-14 23:21:48 +0200

    updated mock api

--- ambitrove-prototype/src/api/paymentGate.ts
+++ ambitrove-prototype/src/api/paymentGate.ts
@@ -1,1 +1,17 @@
 //!  Add stripe API
+import axios from "axios";
+
+const BASE_URL: string = "";
+
+const paymentGateWay = async () => {
+  const response = await axios.get(`https://stripe/api${BASE_URL}`);
+
+  if (response.status === 200) console.log(`Susccesful status ${response}`);
+  try {
+    return null;
+  } catch (error) {
+    console.error(`Sorry seems we something went wrong ${error}`);
+  }
+};
+
+paymentGateWay();
