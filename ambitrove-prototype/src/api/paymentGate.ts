e16f7147743c1fd9ed5a280c02d9148b270e1dfe
Kamogelo <ekmogasoa@gmail.com>
2025-12-14 23:35:26 +0200

    fixed logic

--- ambitrove-prototype/src/api/paymentGate.ts
+++ ambitrove-prototype/src/api/paymentGate.ts
@@ -1,16 +1,18 @@
 //!  Add stripe API
 import axios from "axios";
 
-const BASE_URL: string = "";
+const BASE_URL: string = "https://your-backend.com/api/payment";
 
 const paymentGateWay = async () => {
-  const response = await axios.get(`https://stripe/api${BASE_URL}`);
-
-  if (response.status === 200) console.log(`Susccesful status ${response}`);
   try {
-    return null;
+    const response = await axios.get(BASE_URL);
+    if (response.status === 200)
+      console.log(`Susccesful status ${response.data}`);
+
+    return response.data;
   } catch (error) {
     console.error(`Sorry seems we something went wrong ${error}`);
+    return null;
   }
 };
 
